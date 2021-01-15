0fb99f9b24c9e38faaad2d79ed5b0d912c29eb9d
Yeseo J <kingofthefall538@gmail.com>
2021-01-15 18:38:17 +0900

    Create #2606 바이러스.py

---
+++ 05. BFS & DFS/#2606 바이러스.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 18:34:56 2021
+
+@author: newave986.git
+"""
+
+# BOJ 2606
+
+num1 = int(input())
+num2 = int(input())
+
+connect = [[] for _ in range(num1 + 1)]
+virus = [1]
+
+for i in range(num2):
+    a, b = map(int, input().split())
+    if b not in connect[a]:
+        connect[a].append(b)
+    if a not in connect[b]:
+        connect[b].append(a)
+    
+def DFS(num):
+    global virus
+    global connect
+    
+    for i in connect[num]:
+        if i not in virus:
+            virus.append(i)
+            DFS(i)
+
+BFS(virus[0])
+print(len(virus) - 1)
