b67e5671daf7ccedb71cd1e8d1dcd5aa5e90f7c8
newave986 <kingofthefall538@gmail.com>
2021-01-26 13:35:19 +0900

    #1912 runtime error

---
+++ 08_DynamicProgramming/#1912.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 12:56:58 2021
+
+@author: newave986.git
+"""
+
+# BOJ 1912 연속합 - 다이나믹 프로그래밍 / 동적 계획법
+
+# import sys
+# sys.stdin.readline()
+
+# 런타임 에러
+
+def continuous_sum(x):
+    
+    d = [0] * (x)
+    d[0] = array[0]
+    
+    for i in range(x):
+        d[i] = max(array[i] + d[i-1], array[i])
+    
+    return max(d)
+    
+n = int(input())
+array = list(map(int, (input().split())))
+
+if n == 1:
+    print(array[0])
+else:
+    print(continuous_sum(n))
+
+
+
+
+
