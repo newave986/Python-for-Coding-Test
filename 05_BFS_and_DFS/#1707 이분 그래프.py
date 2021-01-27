cbb45530a16224cdee27da4948fd111f9b4e11db
Yeseo J <kingofthefall538@gmail.com>
2021-01-27 11:52:03 +0900

    Update #1707 이분 그래프.py

--- 05_BFS_and_DFS/#1707 이분 그래프.py
+++ 05_BFS_and_DFS/#1707 이분 그래프.py
@@ -1,31 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 12 17:54:57 2021
+Changed on Wed Jan 27 11:32    2021
 
 @author: newave986.git
 """
 
-import sys as sys
-from collections import deque
+# runtime error
 
-num = int(sys.stdin.readline())
-
-for k in range(num):
+def make_level():
     
-    ver, edg = map(int, sys.stdin.readline().split())
-    my_list = [[] for _ in range(ver+1)]
-    queue = deque()
-    
-    for i in range(edg):
-        a, b = map(int, sys.stdin.readline().split())
-        my_list[a].append(b)
-        my_list[b].append(a)
-    
-    level = [0 for _ in range(ver + 1)]
-    status = True
-    
-    queue.append(1)
-    level[1] = 1
+    global queue
+    global status
     
     while queue:
         
@@ -40,8 +26,40 @@ for k in range(num):
             elif level[t] == level[k]:
                  status = False
                  break
+             
+# import sys
+# sys.stdin.readline()
+
+from collections import deque
+
+num = int(input())
+
+for k in range(num):
     
-    del level[0]
+    ver, edg = map(int, input().split())
+    my_list = [[] for _ in range(ver+1)]
+    queue = deque()
+    
+    for i in range(edg):
+        a, b = map(int, input().split())
+        my_list[a].append(b)
+        my_list[b].append(a)
+    
+    level = [0 for _ in range(ver + 1)]
+    level[0] = 1
+    status = True
+    
+    queue.append(1)
+    level[1] = 1
+    
+    make_level()
+             
+    for l in level:
+        if l == 0:
+            queue.apped(l)
+            level[l] = 1
+            make_level()
+    
     if 0 in level:
         status = False
     
