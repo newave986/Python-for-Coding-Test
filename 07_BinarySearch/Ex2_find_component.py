9e9621949fd5d9c34d511105429abb8a9e2a69eb
newave986 <kingofthefall538@gmail.com>
2021-03-30 22:15:21 +0900

    p.197 Ex2 부품 찾기

---
+++ 07_BinarySearch/Ex2_find_component.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 21:50:21 2021
+
+@author: newave986.git
+"""
+
+# this is coding test with python - book exercise
+# chapter 07 binary search
+# exercise 02 find component
+
+def binary_search(array, start, end, target):
+    
+    if start > end:
+        return None
+    
+    mid = (start + end) // 2
+    
+    if target == array[mid]:
+        return mid
+    
+    if target < array[mid]:
+        return binary_search(array, start, mid - 1, target)
+        
+    else:
+        return binary_search(array, mid + 1, end, target)
+        
+
+# import sys
+# input = sys.stdin.readline
+
+N = int(input())
+stock = list(map(int, input().split()))
+stock.sort()
+
+M = int(input())
+require = list(map(int, input().split()))
+
+for i in require:
+    if binary_search(stock, 0, N-1, i) == None:
+        print("no", end = " ")
+    else:
+        print("yes", end = " ")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
